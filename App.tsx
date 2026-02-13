46c28539fc53e30d6673662db1e07ed2c167be4d
mcylldz <131589219+mcylldz@users.noreply.github.com>
2026-02-13 17:19:31 +0300

    fix: restore /index2 redirect

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import TebriklerPage from './pages/TebriklerPage';
+
+const RedirectToIndex2: React.FC = () => {
+  const location = useLocation();
+  return <Navigate to={`/${location.search}`} replace />;
+};
 
 const App: React.FC = () => {
   return (
@@ -9,6 +13,8 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/tebrikler" element={<TebriklerPage />} />
+        <Route path="/index2" element={<RedirectToIndex2 />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
